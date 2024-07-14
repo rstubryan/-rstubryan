af44e61bc524cf58789794c5fa08851426447612
rstubryan <restubumirr@gmail.com>
2024-07-14 13:24:39 +0700

    init: portfolio project react ts come with rrd and tw css

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LandingPages from "./pages/LandingPages.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPages />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
