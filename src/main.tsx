d405e35e1fb6db4ad4c2faab2c17b3552246518a
rstubryan <restubumirr@gmail.com>
2024-07-20 15:42:34 +0700

    feat(route): add routing on pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import LandingPages from "./pages/LandingPages.tsx";
+import { ThemeProvider } from "@/components/theme-provider.tsx";
+import LandingPages from "@/pages/LandingPages.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <LandingPages />,
   },
+  {
+    path: "/resume",
+    element: <LandingPages />,
+  },
+  {
+    path: "/portfolio",
+    element: <LandingPages />,
+  },
+  {
+    path: "/blog",
+    element: <LandingPages />,
+  },
+  {
+    path: "/contact",
+    element: <LandingPages />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>,
 );
