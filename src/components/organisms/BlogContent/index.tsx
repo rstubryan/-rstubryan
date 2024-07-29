5ae90ff3028f4e299bc399cdd93f312ba0a97414
rstubryan <restubumirr@gmail.com>
2024-07-29 13:17:56 +0700

    chore: blog content coming soon

--- src/components/organisms/BlogContent/index.tsx
+++ src/components/organisms/BlogContent/index.tsx
@@ -1,10 +1,13 @@
 import ContentTitle from "@/components/molecules/ContentTitle";
+import BaseText from "@/components/atoms/BaseText";
 
 export default function BlogContent() {
   return (
     <>
       <ContentTitle heading={`Blog`} description={``} />
-      <div className=""></div>
+      <div className="container mx-auto flex items-center justify-center">
+        <BaseText className={`text-center`}>Coming soon</BaseText>
+      </div>
     </>
   );
 }
